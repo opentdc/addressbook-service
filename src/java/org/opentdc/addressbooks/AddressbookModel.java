7e0d3effc8e6eab612f480136ef9e110adefbff3
Bruno Kaiser <bruno@bkaiser.ch>
2015-04-26 20:10:50 +0200

    removed wrong comment

--- src/java/org/opentdc/addressbooks/AddressbookModel.java
+++ src/java/org/opentdc/addressbooks/AddressbookModel.java
@@ -27,10 +27,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * Present
- *
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
 public class AddressbookModel {
